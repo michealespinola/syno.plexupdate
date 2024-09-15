caf2ec619cb351c94c44121efe274fe58ecc87f7
Micheal Espinola <michealespinola@users.noreply.github.com>
2024-09-14 22:53:03 -0700

    Major organizational overhaul
    * Renamed most variables
    * In-line documented all variables
    * Reorganize summary output

--- Tools/syno.plexinfo.sh
+++ Tools/syno.plexinfo.sh
@@ -3,82 +3,90 @@
 # SC1091,SC2004,SC2154,SC2181
 # bash /volume1/homes/admin/scripts/bash/plex/syno.plexupdate/syno.plexinfo.sh
 
-# SCRIPT VERSION
-SPIScrpVer=1.1.0
+SCRIPT_VERSION=2.0.0
 
-# Function to get DSM version
-get_dsm_version() {
-  SynoDSMVer=$(grep -i "productversion=" "/etc.defaults/VERSION" | cut -d"\"" -f 2)
-  SynoDSMVer=$(echo "$SynoDSMVer"-"$(grep -i "buildnumber=" "/etc.defaults/VERSION" | cut -d"\"" -f 2)")
-  SynoDSMUpV=$(grep -i "smallfixnumber=" "/etc.defaults/VERSION" | cut -d"\"" -f 2)
-  if [ -n "$SynoDSMUpV" ]; then
-    SynoDSMVer="$SynoDSMVer Update $SynoDSMUpV"
-  fi
+# Function to get Script information
+get_src_info() {
+  srcScrpVer=${SCRIPT_VERSION}                                                                    # Source Script Version
+  srcFullPth=$(readlink -f "${BASH_SOURCE[0]}")                                                   # Source Script Absolute Path Of Script
+  srcDirctry=$(dirname "$srcFullPth")                                                             # Source Script Directory Containing Script
+  srcFileNam=${srcFullPth##*/}                                                                    # Source Script Script File Name
 }
 
 # Function to get NAS information
 get_nas_info() {
-  SynoHModel=$(cat /proc/sys/kernel/syno_hw_version)
-  SynoFmArch=$(uname --machine)
-  SynoFmProc=$(uname --all | awk '{ print $NF }' | awk -F "_" '{ print $2 }')
-  SynoNodenm=$(uname --nodename)
-  SynoFmKern=$(uname --kernel-name)
-  SynoFmKrnR=$(uname --kernel-release)
-  SynoFmKLcs=$(echo "$SynoFmKern" | awk '{ print tolower($0) }')
-  SynoBashVr=$(bash --version | head -n 1 | awk '{print $4}')
-  SynoTmZone=$(readlink /etc/localtime | sed 's|/usr/share/zoneinfo/||')
-
-  SynoAdminX=$(synouser --get admin | grep "Expired" | awk -F "[][{}]" '{ print $2 }')
-  if [ "$SynoAdminX" = "true" ]; then
-    SynoAdminX="Disabled"
-  elif [ "$SynoAdminX" = "false" ]; then
-    SynoAdminX="Enabled (SECURITY RISK)"
+  nasHwModel=$(cat /proc/sys/kernel/syno_hw_version)                                              # NAS Model
+  nasMchArch=$(uname --machine)                                                                   # NAS Machine Architecture
+  [ "$nasMchArch" = "i686" ] && nasMchArch=x86                                                    # "-" Override Match
+  [ "$nasMchArch" = "armv7l" ] && nasMchArch=armv7neon                                            # "-" Override Match
+  nasMchProc=$(uname --all | awk '{ print $NF }' | awk -F "_" '{ print $2 }')                     # NAS Processor Platform
+  nasNodeNam=$(uname --nodename)                                                                  # NAS Network Hostname
+  nasMchKern=$(uname --kernel-name)                                                               # NAS Kernel Name
+  nasMchKver=$(uname --kernel-release)                                                            # NAS Kernel Version
+  nasMchKlcs=$(echo "$nasMchKern" | awk '{ print tolower($0) }')                                  # NAS Kernel Name (lowercase)
+  nasBashVer=$(bash --version | head -n 1 | awk '{print $4}')                                     # NAS Bash Version
+  nasTimZone=$(readlink /etc/localtime | sed 's|/usr/share/zoneinfo/||')                          # NAS Time Zone Configuration
+  nasAdminXp=$(synouser --get admin | grep "Expired" | awk -F "[][{}]" '{ print $2 }')            # NAS Admin Account Check
+  if [ "$nasAdminXp" = "true" ]; then
+    nasAdminXp="Disabled"
+  elif [ "$nasAdminXp" = "false" ]; then
+    nasAdminXp="Enabled (SECURITY RISK)"
   else
-    SynoAdminX="Unknown (???)"
+    nasAdminXp="Unknown (???)"
   fi
-
-  SynoIntrIP=$(ip -f inet -o addr show eth0 | awk '{ print $4 }' | cut -d/ -f1)
-  SynoExtrIP=$(ping -c 1 myip.opendns.com | awk -F "[()]" '/PING/ { print $2 }')
+  nasIntrnIP=$(ip -f inet -o addr show eth0 | awk '{ print $4 }' | cut -d/ -f1)                   # NAS Internal IP Address (eth0)
+  nasExtrnIP=$(ping -c 1 myip.opendns.com   | awk -F "[()]" '/PING/ { print $2 }')                # NAS External IP Address
 }
 
-# Function to get Plex Media Server information
-get_plex_info() {
-  PlexMSVers=$(/usr/syno/bin/synopkg version "PlexMediaServer")
-  PlexTarget=$(readlink /var/packages/PlexMediaServer/target)
-  PlexAppBin="$PlexTarget"
-  PlexShares=$(readlink /var/packages/PlexMediaServer/shares/PlexMediaServer)
-  PlexFolder="$PlexShares"
-  PlexFolder="$PlexFolder/AppData/Plex Media Server"
-  PlexTransc=$("$PlexAppBin/Plex Transcoder" -version -hide_banner | head -n 1 | cut -d " " -f 1)
-  PlexTransV=$("$PlexAppBin/Plex Transcoder" -version -hide_banner | head -n 1 | cut -d " " -f 3)
-  PlexCodecs="$PlexFolder/Codecs"
-  PlexFVCdcs=$(find "$PlexFolder/Codecs" -type d -name "$PlexTransV-$SynoFmKLcs-$SynoFmArch")
-  PlexDevcID=$(head -n 1 "$PlexCodecs/.device-id")
-  PlexFrName=$(grep -oP "FriendlyName=\"\K[^\"]+" "$PlexFolder/Preferences.xml")
-  PlexOToken=$(grep -oP "PlexOnlineToken=\"\K[^\"]+" "$PlexFolder/Preferences.xml")
-  PlexMachID=$(grep -oP "ProcessedMachineIdentifier=\"\K[^\"]+" "$PlexFolder/Preferences.xml")
-  PlexChannl=$(grep -oP "ButlerUpdateChannel=\"\K[^\"]+" "$PlexFolder/Preferences.xml")
-  if [ -z "$PlexChannl" ]; then
-    PlexChannl=Public
+# Function to get DSM information
+get_dsm_info() {
+  dsmPrdctNm=$(grep -i "productversion=" "/etc.defaults/VERSION" | cut -d"\"" -f 2)               # DSM Product Version
+  dsmBuildNm=$(grep -i "buildnumber="    "/etc.defaults/VERSION" | cut -d"\"" -f 2)               # DSM Build Number
+  dsmMinorVr=$(grep -i "smallfixnumber=" "/etc.defaults/VERSION" | cut -d"\"" -f 2)               # DSM Minor Version
+  if [ -n "$dsmMinorVr" ]; then
+    dsmFullVer="$dsmPrdctNm-$dsmBuildNm Update $dsmMinorVr"
   else
-    if [ "$PlexChannl" -eq "0" ]; then
-      PlexChannl=Public
-    elif [ "$PlexChannl" -eq "8" ]; then
-      PlexChannl=Beta
+    dsmFullVer="$dsmPrdctNm-$dsmBuildNm"
+  fi
+}
+
+# Function to get PMS Media Server information
+get_pms_info() {
+  pmsVersion=$(synopkg version "PlexMediaServer")                                                 # PMS Version
+  pmsSTarget=$(readlink /var/packages/PlexMediaServer/target)                                     # PMS Target Symbolic Link
+  pmsApplDir="$pmsSTarget"                                                                        # PMS Application Directory
+  pmsSShares=$(readlink /var/packages/PlexMediaServer/shares/PlexMediaServer)                     # PMS Shares Symbolic Link
+  pmsDataDir="$pmsSShares/AppData/Plex Media Server"                                              # PMS Data Directory
+  pmsTrnscdr=$("$pmsApplDir/Plex Transcoder" -version -hide_banner | head -n 1 | cut -d " " -f 1) # PMS Transcoder App
+  pmsTrnscdV=$("$pmsApplDir/Plex Transcoder" -version -hide_banner | head -n 1 | cut -d " " -f 3) # PMS Transcoder Version
+  pmsCdcsDir="$pmsDataDir/Codecs"                                                                 # PMS Codecs Directory
+  pmsCdcVDir=$(find "$pmsDataDir/Codecs" -type d -name "$pmsTrnscdV-$nasMchKlcs-$nasMchArch")     # PMS Transcoder Version Codecs Directory
+  pmsFrnName=$(grep -oP "FriendlyName=\"\K[^\"]+" "$pmsDataDir/Preferences.xml")                  # PMS Friendly Name
+  pmsDevicID=$(head -n 1 "$pmsCdcsDir/.device-id")                                                # PMS Device ID
+  pmsMachnID=$(grep -oP "ProcessedMachineIdentifier=\"\K[^\"]+" "$pmsDataDir/Preferences.xml")    # PMS Machine ID
+  pmsOnToken=$(grep -oP "PlexOnlineToken=\"\K[^\"]+" "$pmsDataDir/Preferences.xml")               # PMS Online Token
+  pmsChannel=$(grep -oP "ButlerUpdateChannel=\"\K[^\"]+" "$pmsDataDir/Preferences.xml")           # PMS Update Channel
+  if [ -z "$pmsChannel" ]; then
+    pmsChannel=Public
+  else
+    if [ "$pmsChannel" -eq "0" ]; then
+      pmsChannel=Public
+    elif [ "$pmsChannel" -eq "8" ]; then
+      pmsChannel=Beta
     else
-      PlexChannl=Undefined
+      pmsChannel=Undefined
     fi
   fi
-  PlexAutoET=$(grep -oP "autoEmptyTrash=\"\K[^\"]+" "$PlexFolder/Preferences.xml")
-  if [ -z "$PlexAutoET" ]; then
-    PlexAutoET="Not Automatic"
+  pmsAutTrsh=$(grep -oP "autoEmptyTrash=\"\K[^\"]+" "$pmsDataDir/Preferences.xml")                # PMS Auto Empty Trash
+  if [ -z "$pmsAutTrsh" ]; then
+    pmsAutTrsh="Not Automatic"
   else
-    if [ "$PlexAutoET" -eq "0" ]; then
-      PlexAutoET="Not Automatic"
-    elif [ "$PlexAutoET" -eq "1" ]; then
-      PlexAutoET="Automatic (DISCONNECTION RISK)"
+    if [ "$pmsAutTrsh" -eq "0" ]; then
+      pmsAutTrsh="Not Automatic"
+    elif [ "$pmsAutTrsh" -eq "1" ]; then
+      pmsAutTrsh="Automatic (DISCONNECTION RISK)"
     else
-      PlexAutoET="Undefined (???)"
+      pmsAutTrsh="Undefined (???)"
     fi
   fi
 }
@@ -86,58 +94,55 @@ get_plex_info() {
 # Function to summarize data
 print_summary() {
   # PRINT OUR GLORIOUS HEADER BECAUSE WE ARE FULL OF OURSELVES
-  printf "\n"
-  printf "%s\n" "SYNO.PLEX INFO SCRIPT for DSM 7"
-  printf "\n"
+  printf "\n%s\n\n" "SYNO.PLEX INFO SCRIPT for DSM 7"
 
   # Call functions to gather information
-  get_dsm_version
+  get_src_info
   get_nas_info
-  get_plex_info
+  get_dsm_info
+  get_pms_info
 
   # Print the collected information
   printf "SYNOLOGY NAS INFO:\n"
   printf "%s\n" "---------------"
-  printf "       Nodename: %s\n" "$SynoNodenm"
-  printf "        DSM ver: %s\n" "$SynoDSMVer"
-  printf "          Model: %s\n" "$SynoHModel"
-  printf "   Architecture: %s (%s)\n" "$SynoFmArch" "$SynoFmProc"
-  printf "         Kernel: %s (%s)\n" "$SynoFmKern" "$SynoFmKrnR"
-  printf "           Bash: %s\n" "$SynoBashVr"
-  printf "      Time Zone: %s\n" "$SynoTmZone"
-  printf "  Admin account: %s\n" "$SynoAdminX"
-  printf "    Internal IP: %s\n" "$SynoIntrIP"
-  printf "    External IP: %s\n" "$SynoExtrIP"
+  printf '%16s %s\n'       "Nodename:" "$nasNodeNam"
+  printf '%16s %s\n'        "DSM ver:" "$dsmFullVer"
+  printf '%16s %s\n'          "Model:" "$nasHwModel"
+  printf '%16s %s\n'   "Architecture:" "$nasMchArch ($nasMchProc)"
+  printf '%16s %s\n'         "Kernel:" "$nasMchKern ($nasMchKver)"
+  printf '%16s %s\n'           "Bash:" "$nasBashVer"
+  printf '%16s %s\n'      "Time Zone:" "$nasTimZone"
+  printf '%16s %s\n'  "Admin account:" "$nasAdminXp"
+  printf '%16s %s\n'    "Internal IP:" "$nasIntrnIP"
+  printf '%16s %s\n'    "External IP:" "$nasExtrnIP"
   printf "\n"
 
   printf "PLEX MEDIA SERVER INFO:\n"
   printf "%s\n" "---------------"
-  printf "  Friendly Name: %s\n" "$PlexFrName"
-  printf "        PMS ver: %s\n" "$PlexMSVers"
-  printf " Update Channel: %s\n" "$PlexChannl"
-  printf "    Empty Trash: %s\n" "$PlexAutoET"
-  printf "     Transcoder: %s (%s)\n" "$PlexTransc" "$PlexTransV"
-
-  printf "\n"
-  printf "PLEX MEDIA SERVER ID (DO NOT SHARE):\n"
-  printf "%s\n" "---------------"
-  printf "      Device-ID: %s\n" "$PlexDevcID"
-  printf "     Machine-ID: %s\n" "$PlexMachID"
-  printf "   Online Token: %s\n" "$PlexOToken"
+  printf '%16s %s\n'  "Friendly Name:" "$pmsFrnName"
+  printf '%16s %s\n'        "PMS ver:" "$pmsVersion"
+  printf '%16s %s\n' "Update Channel:" "$pmsChannel"
+  printf '%16s %s\n'    "Empty Trash:" "$pmsAutTrsh"
+  printf '%16s %s\n'     "Transcoder:" "$pmsTrnscdr ($pmsTrnscdV)"
   printf "\n"
 
   printf "PLEX DIRECTORY REFERENCE:\n"
   printf "%s\n" "---------------"
-  [ -d "$PlexAppBin"                 ] && printf "'%s'\n" "$PlexAppBin"
-  [ -d "$PlexFolder"                 ] && printf "'%s'\n" "$PlexFolder"
-  [ -d "$PlexFolder/Codecs"          ] && printf "  \"  \'/Codecs\'\n"
-  [ -d "$PlexFVCdcs"                 ] && printf "  \"  \'/Codecs/%s-%s-%s\'\n" "$PlexTransV" "$SynoFmKLcs" "$SynoFmArch"
-  [ -d "$PlexFolder/Logs"            ] && printf "  \"  \'/Logs\'\n"
-  [ -d "$PlexFolder/Plug-ins"        ] && printf "  \"  \'/Plug-ins\'\n"
-  [ -d "$PlexFolder/Plug-in Support" ] && printf "  \"  \'/Plug-in Support\'\n"
-  [ -d "$PlexFolder/Scanners"        ] && printf "  \"  \'/Scanners\'\n"
-  [ -d "$PlexFolder/Scanners/Common" ] && printf "  \"  \'/Scanners/Common\'\n"
-  [ -d "$PlexFolder/Scanners/Series" ] && printf "  \"  \'/Scanners/Series\'\n"
+  [ -d "$pmsApplDir" ]                 && printf '%16s %s\n'  "Applications:" "$pmsApplDir"
+  [ -d "$pmsDataDir" ]                 && printf '%16s %s\n'       "AppData:" "$pmsDataDir"
+  [ -d "$pmsDataDir/Cache" ]           && printf '%16s %s\n'         "Cache:" " \" /Cache"
+  [ -d "$pmsCdcVDir" ]                 && printf '%16s %s\n'        "Codecs:" " \" /Codecs/$pmsTrnscdV-$nasMchKlcs-$nasMchArch"
+  [ -d "$pmsDataDir/Logs" ]            && printf '%16s %s\n' "Crash Reports:" " \" /Crash Reports"
+  [ -d "$pmsDataDir/Logs" ]            && printf '%16s %s\n'          "Logs:" " \" /Logs"
+  [ -d "$pmsDataDir/Plug-ins" ]        && printf '%16s %s\n'      "Plug-ins:" " \" /Plug-ins"
+  [ -d "$pmsDataDir/Scanners" ]        && printf '%16s %s\n'      "Scanners:" " \" /Scanners"
+  printf "\n"
+
+  printf "PLEX MEDIA SERVER IDs (DO NOT SHARE):\n"
+  printf "%s\n" "---------------"
+  printf '%16s %s\n'      "Device-ID:" "$pmsDevicID"
+  printf '%16s %s\n'     "Machine-ID:" "$pmsMachnID"
+  printf '%16s %s\n'   "Online Token:" "$pmsOnToken"
   printf "%s\n" "---------------"
   printf "\n"
 }
